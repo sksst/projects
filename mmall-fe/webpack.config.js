c49f6b4503bfd7543e14b17c233792445d9c2579
sksst <sksst007@gmail.com>
2018-04-10 00:27:19 +0800

    update password

--- mmall-fe/webpack.config.js
+++ mmall-fe/webpack.config.js
@@ -40,7 +40,7 @@ var config = {
         'user-pass-reset'   : ['./src/page/user-pass-reset/index.js'],
         'user-center'       : ['./src/page/user-center/index.js'],
         'user-center-update': ['./src/page/user-center-update/index.js'],
-        // 'user-pass-update'  : ['./src/page/user-pass-update/index.js'],
+        'user-pass-update'  : ['./src/page/user-pass-update/index.js'],
         'result'            : ['./src/page/result/index.js'],
         // 'about'             : ['./src/page/about/index.js'],
     },
@@ -97,7 +97,7 @@ var config = {
         new HtmlWebpackPlugin(getHtmlConfig('user-pass-reset', '找回密码')),
         new HtmlWebpackPlugin(getHtmlConfig('user-center', '个人中心')),
         new HtmlWebpackPlugin(getHtmlConfig('user-center-update', '修改个人信息')),
-        // new HtmlWebpackPlugin(getHtmlConfig('user-pass-update', '修改密码')),
+        new HtmlWebpackPlugin(getHtmlConfig('user-pass-update', '修改密码')),
         new HtmlWebpackPlugin(getHtmlConfig('result', '操作结果')),
         // new HtmlWebpackPlugin(getHtmlConfig('about', '关于MMall')),
     ]
